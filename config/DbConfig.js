fe3de535e3552e054a99e93965d2b23e3f88b566
sharjeel0288 <k200288@nu.edu.pk>
2023-08-25 12:34:18 +0500

    updated

--- config/DbConfig.js
+++ config/DbConfig.js
@@ -34,6 +34,7 @@ const pool = mysql.createPool({
         password VARCHAR(255),    
         email VARCHAR(255)
       );
+      
       CREATE TABLE IF NOT EXISTS payment_mode (
         id INT PRIMARY KEY AUTO_INCREMENT,
         name VARCHAR(255),
